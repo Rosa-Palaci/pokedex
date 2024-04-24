3a018961b0f2094c206fd1af8de38ec959d63273
Rosa-Palaci <palacibelrose431@gmail.com>
2024-04-24 15:57:35 -0600

    nueva version 1 del pokedex

--- js/buscador.js
+++ js/buscador.js
@@ -1,30 +1,3 @@
-const display = document.getElementById('display');
-const pokemonNumber = document.getElementById('pokemonNumber');
-const apiButton = document.getElementById('apiButton');
-const clearButton = document.getElementById('clearButton'); 
-const buttons = document.querySelectorAll('#keypad button');
-
-function appendToDisplay(number) {
-    
-    pokemonNumber.textContent += number;
-    display.textContent = pokemonNumber.textContent; 
-
-}
-
-function clearDisplay() {
-  pokemonNumber.textContent = '';
-  display.textContent = ''; 
-}
-
-buttons.forEach(button => {
-  button.addEventListener('click', (event) => {
-    appendToDisplay(event.target.textContent);
-  });
-});
-
-apiButton.addEventListener('click', searchPokemonData);
-clearButton.addEventListener('click', clearDisplay); 
-
 function searchPokemonData() {
   const pokemonId = pokemonNumber.textContent;
   const apiUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonId}`;
@@ -41,3 +14,35 @@ function searchPokemonData() {
       display.textContent = 'Error al buscar el Pokémon';
     });
 }
+
+document.getElementById('search').addEventListener('keypress', function(event) {
+  if (event.key === 'Enter') {
+      event.preventDefault(); // Previene la funcionalidad por defecto del Enter
+
+      // Obtiene el valor ingresado en el input
+      var pokemonId = this.value.toLowerCase();
+
+      // Crea la URL para la PokeAPI
+      var apiUrl = 'https://pokeapi.co/api/v2/pokemon/' + pokemonId;
+
+      // Llama a la API para obtener información del Pokémon
+      fetch(apiUrl)
+          .then(response => {
+              if (response.ok) {
+                  return response.json();
+              }
+              throw new Error('No se encontró el Pokémon.');
+          })
+          .then(data => {
+              var pokemonName = data.name; // Nombre del Pokémon
+              var pokemonImage = data.sprites.front_default; // Imagen del Pokémon
+
+              // Actualiza el div screen_pokemon con la imagen y el nombre
+              document.getElementById('screen').innerHTML = '<img src="' + pokemonImage + '" alt="' + pokemonName + '"><p>' + pokemonName + '</p>';
+          })
+          .catch(error => {
+              console.error('Error:', error);
+              document.getElementById('screen').innerHTML = '<p>Pokémon no encontrado. Intente con otro nombre o ID.</p>';
+          });
+  }
+});
